3bb414bc7725bcd7a8002b4dbb9b71b425df42c8
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-05 21:56:49 -0500

    add code to bot

--- Gemfile
+++ Gemfile
@@ -1,1 +1,6 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'rubocop'
 gem 'telegram-bot-ruby'
--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
-require_relative '../lib/bot.rb'
-token = "1473916105:AAELTf_XfjmmePztuIKTfnir_AXJTe8woMo"
+# frozen_string_literal: true
+
+require_relative '../lib/bot'
+token = '1473916105:AAELTf_XfjmmePztuIKTfnir_AXJTe8woMo'
 Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-        input = message.text
-        p input + " otro linea"
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, I´m a bot")
-    end
-    end
+  bot.listen do |message|
+    input = message.text
+    p "#{input} otro linea"
+    bot.api.send_message(chat_id: message.chat.id, text: 'Hello, I´m a bot')
+  end
+end
