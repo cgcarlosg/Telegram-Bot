de07325009047853b5ef798cba6b3da461ba8ed8
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-07 23:05:50 -0500

    fix indentation according to linters report

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,7 +1,7 @@
 require './lib/bot'
 
 describe '#printerfinder_bot' do
-   it 'should not return a message if argument is empty' do
+  it 'should not return a message if argument is empty' do
     expect(@input = nil).to eql(nil)
   end
 end
